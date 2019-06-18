96f5b3e010533e59b114788aaf4eb3b2f9d0ee40
NeerajLouji <loujin@sheridancollege.ca>
2019-06-18 12:28:13 -0400

    Demo project57

---
+++ src/projectone/ArrayNew.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectone;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Neeraj
+ * 
+ */
+public class ArrayNew {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner input1=new Scanner(System.in);
+    String word=input1.next();
+    System.out.println("Enter a word");
+   
+    
+    
+    char[] name=new char[word.length()];
+    
+    for(int i=0;i<word.length();i++){
+    
+      name[i]=word.charAt(i);
+}
+    for(int i=word.length();i>=0;i--){
+    
+    System.out.println(name[i]);
+    }
+    
+
+    }
+}
---
+++ src/projectone/ProjectOne.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectone;
+
+/**
+ *
+ * @author Neeraj
+ */
+public class ProjectOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        char[] n=new char[5];
+        
+        n[0]='h';
+         n[1]='e';
+          n[2]='l';
+           n[3]='l';
+            n[4]='o';
+            
+     for(int i=0;i<n.length;i++){
+         System.out.print(n[i]);
+     
+     }       
+        
+    }
+    
+}
